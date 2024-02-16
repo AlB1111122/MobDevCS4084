fa90611ef1e0514b30a25ce24f270e530783c893
Alex <21338787@studentmail.ul.ie>
2024-02-16 15:16:15 +0000

    add firebase and auth dependecies/libs

--- app/app/src/main/java/ul/ie/cs4084/app/MainActivity.java
+++ app/app/src/main/java/ul/ie/cs4084/app/MainActivity.java
@@ -1,8 +1,16 @@
 package ul.ie.cs4084.app;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 
 public class MainActivity extends AppCompatActivity {
 
