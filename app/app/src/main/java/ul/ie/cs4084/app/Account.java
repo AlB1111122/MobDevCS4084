f7cba0f6f9b897fe8bb03f8c8e83a0cdbff61dd7
Alex <21338787@studentmail.ul.ie>
2024-02-25 16:59:54 +0000

    add a check to the editing the profile picture process

--- app/app/src/main/java/ul/ie/cs4084/app/Account.java
+++ app/app/src/main/java/ul/ie/cs4084/app/Account.java
@@ -1,14 +1,34 @@
 package ul.ie.cs4084.app;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
 public class Account {
-    private String authUuid;
+    private final String authUuid;
     private String username;
     private String profilePictureUrl;
 
-    public Account(String authUuid, String username){
+    private ArrayList<String> followedTags;
+    private ArrayList<String> blockedTags;
+    public Account(String authUuid){
         this.authUuid = authUuid;
-        this.username = username;
         this.profilePictureUrl = "gs://socialmediaapp-38b04.appspot.com/profilePictures/defaultProfile.jpg";
+    }
+
+    public void setAttributes(String username, String profilePictureUrl, ArrayList<String> followedTags, ArrayList<String> blockedTags){
+        this.username = username;
+        this.profilePictureUrl = profilePictureUrl;
+        this.followedTags = followedTags;
+        this.blockedTags = blockedTags;
     }
 
     public String getAuthUuid(){
@@ -21,6 +41,32 @@ public class Account {
 
     public String getProfilePictureUrl() {
         return this.profilePictureUrl;
+    }
+
+    public ArrayList<String> getFollowedTags() {
+        return followedTags;
+    }
+
+    public ArrayList<String> getBlockedTags() {
+        return blockedTags;
+    }
+
+    public void saveToDb() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("accounts").document(this.authUuid)
+                .set(this)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
     }
 }
 
--- app/app/src/main/java/ul/ie/cs4084/app/LandingActivity.java
+++ app/app/src/main/java/ul/ie/cs4084/app/LandingActivity.java
@@ -24,6 +24,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
@@ -31,6 +33,7 @@ import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class LandingActivity extends AppCompatActivity {
@@ -44,51 +47,53 @@ public class LandingActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user != null) {
-            Account testAccount = new Account(user.getUid(), "testAccount");
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            Log.d(TAG, Objects.requireNonNull(user.getDisplayName()));
-            db.collection("accounts").document(testAccount.getAuthUuid())
-                    .set(testAccount)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d(TAG, "DocumentSnapshot successfully written!");
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w(TAG, "Error writing document", e);
-                        }
-                    });
-            ImageView pfp = (ImageView)findViewById(R.id.imageView);
-            //FirebaseStorage storageRef = FirebaseStorage.getInstance();
-            StorageReference gsReference = FirebaseStorage.getInstance().getReferenceFromUrl(testAccount.getProfilePictureUrl());
-
-            final long ONE_MEGABYTE = 1024 * 1024;
-
-            gsReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-                @Override
-                public void onSuccess(byte[] bytes) {
-                    Drawable image = new BitmapDrawable(getResources(), BitmapFactory.decodeByteArray(
-                            bytes,
-                            0,
-                            bytes.length
-                    ));
-                    pfp.setImageDrawable(image);
+        //get the user who just signed in
+        FirebaseUser fireBaseAuthUser = FirebaseAuth.getInstance().getCurrentUser();
+        assert fireBaseAuthUser != null; // we know its not null because they just signed in
+        //make an object to represent the Account
+        Account signedInAccount = new Account(fireBaseAuthUser.getUid());
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //check if the Account for this fireBaseAuthUser exists
+        DocumentReference signedInUser = db.collection("accounts").document(fireBaseAuthUser.getUid());
+        signedInUser.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    //if yes fully populate local object
+                    DocumentSnapshot document = task.getResult();
+                    signedInAccount.setAttributes(
+                            (String) document.get("username"),
+                            (String) document.get("profilePictureUrl"),
+                            (ArrayList<String>) document.get("followedTags" ),
+                            (ArrayList<String>) document.get("blockedTags" )
+                    );
+                    if (!document.exists()) {
+                        //if not create a document in the db
+                        signedInAccount.saveToDb();
+                    }
                 }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception exception) {
-                    // Handle any errors
-                }
-            });
-        }else{
-            Log.d(TAG, "sign in failed");
-            return;
-        }
+            }
+        });
+        //display the pfp on screen
+        ImageView pfp = (ImageView)findViewById(R.id.imageView);
+        StorageReference gsReference = FirebaseStorage.getInstance().getReferenceFromUrl(signedInAccount.getProfilePictureUrl());
+        final long ONE_MEGABYTE = 1024 * 1024;
+        gsReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Drawable image = new BitmapDrawable(getResources(), BitmapFactory.decodeByteArray(
+                        bytes,
+                        0,
+                        bytes.length
+                ));
+                pfp.setImageDrawable(image);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.d(TAG, "error fetching defaultPFP");
+            }
+        });
     }
 
     public void signOut(View view){
