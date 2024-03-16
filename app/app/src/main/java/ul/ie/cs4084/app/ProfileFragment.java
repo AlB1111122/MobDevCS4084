ed6c94533ac42badebb7f92abeefa004c8ed105c
Alex Butler <101115956+AlB1111122@users.noreply.github.com>
2024-03-16 18:03:32 +0000

    Replace activities with fragments (#10)
    * switched signing in and out over to fragments
    
    * profile fragment refined but activity landing left in the project as an example

--- app/app/src/main/java/ul/ie/cs4084/app/ProfileFragment.java
+++ app/app/src/main/java/ul/ie/cs4084/app/ProfileFragment.java
@@ -1,16 +1,24 @@
 package ul.ie.cs4084.app;
-
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -24,29 +32,57 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.HashSet;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import ul.ie.cs4084.app.dataClasses.Account;
-//NOTE::NOT USED ANYMORE LEFT IN AS EXAMPLE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-public class LandingActivity extends AppCompatActivity {
-    ExecutorService executorService;
-    Account signedInAccount;
-    FirebaseFirestore db;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
-        int NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors();
-        executorService = Executors.newFixedThreadPool(NUMBER_OF_CORES);
-        db = FirebaseFirestore.getInstance();
+
+public class ProfileFragment extends Fragment {
+    private
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private Account signedInAccount;
+    private ImageView pfp;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
     }
 
     @Override
-    protected void onStart() {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container,false);
+        pfp = (ImageView) view.findViewById(R.id.imageView);
+        Button signOutB = (Button) view.findViewById(R.id.button2);
+        NavController navController = NavHostFragment.findNavController(this);
+        signOutB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(getActivity())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // user is now signed out
+                                navController.navigate(R.id.signIn);//TODO: change where signing out happens
+                            }
+                        });
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onStart() {
         super.onStart();
-        //display the pfp on screen
-        ImageView pfp = (ImageView)findViewById(R.id.imageView);
         //get the user who just signed in
         FirebaseUser fireBaseAuthUser = FirebaseAuth.getInstance().getCurrentUser();
         assert fireBaseAuthUser != null; // we know its not null because they just signed in
@@ -102,91 +138,8 @@ public class LandingActivity extends AppCompatActivity {
                         Log.d(TAG, "error fetching defaultPFP");
                     }
                 });
-                //test post creation
-                /*ArrayList<String> rules = new ArrayList<>();
-                rules.add("rule1");
-                rules.add("rule2");
-                rules.add("rule3");
-
-                HashSet<String> tags = new HashSet<String>();
-                tags.add("a");
-                tags.add("v");
-                tags.add("f");
-                tags.add("a");
-
-                DocumentReference q = db.document("accounts/eAq9tvZUAqdxM1Me2ytvP1jsoXy2");
-                //DocumentReference w = db.document("accounts/eAq9tvZUAqdxM1Me2ytvP1jsoXy2");
-
-                DocumentReference z = db.document("accounts/UoXcHOdoVVnGe0bJgtpz");
-                DocumentReference x = db.document("accounts/YZjRucblHUvEWIs30iE1");
-
-                HashSet<DocumentReference> mods = new HashSet<>();
-                mods.add(q);
-                //mods.add(w);
-                HashSet<DocumentReference> voters = new HashSet<>();
-                voters.add(z);
-                voters.add(x);
-
-
-                Factory factory = new Factory(executorService);
-
-                CollectionReference boards = db.collection("boards");
-                DocumentReference testBoard =
-                        boards.document("n5rfl0XL6Wwo8vfcGLux");
-
-                testBoard.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot snapshot) {
-                        Board b = new Board(snapshot);
-                        b.removeModerators(q,db);
-                        b.addTag("womp",db);
-                    }
-                });*/
             }
         });
     }
-
-
-        /*factory.createNewPost(
-                db,
-                testBoard,
-                q,
-                "test  threded jaNHVHJBVIFUYSGDHVOHU",
-                "body of the post made on a thred2NJAVBUIODJKVNBUOIAHJNIOVDA",
-                null,
-                tags,
-                new DBCallback<DocumentReference>() {
-                    @Override
-                    public void callback(DocumentReference documentReference) {
-                        factory.createNewComment(documentReference, x, "String bodyNVJIBDIOXHB(UGDIOAVHJBGOIUDAHKJXBUIHV from thred2",new DBCallback<DocumentReference>() {
-                            @Override
-                            public void callback(DocumentReference documentReference) {
-                                Log.d(TAG, "fjiewjfwjfoifejfjwjf");
-                            }
-                        });
-                    }
-                }
-        );
-            }
-        });*/
-
-        /*StorageReference gsReference = FirebaseStorage.getInstance().getReferenceFromUrl(signedInAccount.getProfilePictureUrl());
-        final long ONE_MEGABYTE = 1024 * 1024;
-        gsReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-            @Override
-            public void onSuccess(byte[] bytes) {
-                Drawable image = new BitmapDrawable(getResources(), BitmapFactory.decodeByteArray(
-                        bytes,
-                        0,
-                        bytes.length
-                ));
-                pfp.setImageDrawable(image);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                Log.d(TAG, "error fetching defaultPFP");
-            }
-        });*/
-
 }
+
