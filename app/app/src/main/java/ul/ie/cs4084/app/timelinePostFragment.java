b20394df5dae4bb60fbd565cf13bf8b506a44dd8
Alex <21338787@studentmail.ul.ie>
2024-04-04 16:17:06 +0100

    timeline fragment done

--- app/app/src/main/java/ul/ie/cs4084/app/timelinePostFragment.java
+++ app/app/src/main/java/ul/ie/cs4084/app/timelinePostFragment.java
@@ -2,26 +2,38 @@ package ul.ie.cs4084.app;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
 
 import ul.ie.cs4084.app.dataClasses.Post;
 
 public class timelinePostFragment extends Fragment {
 
-    TextView postTitle;
-    TextView postBody;
-    Button upvote;
-    Button downvote;
+    private TextView postTitle;
+    private TextView postBody;
+    private Button upvote;
+    private Button downvote;
+    private Handler mainHandler;
+    private NavController navController;
+    private ExecutorService executor;
     public timelinePostFragment() {
         // Required empty public constructor
     }
@@ -33,36 +45,65 @@ public class timelinePostFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        MainActivity act = (MainActivity)getActivity();
+        assert act != null;
+        executor = act.executorService;//DEF not how to do it firegure out later
+        mainHandler = new Handler(Looper.getMainLooper());
+        navController = NavHostFragment.findNavController(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_timeline_post, container, false);
+        executor.execute(()->{
+        // Inflate the layout for this fragment
         postTitle = view.findViewById(R.id.postTitleText);
         postBody = view.findViewById(R.id.postBody);
         upvote = view.findViewById(R.id.upvoteButton);
         downvote = view.findViewById(R.id.downvoteButton);
+        TextView upCount = view.findViewById(R.id.upvoteCounter);
+        TextView downCount = view.findViewById(R.id.downvoteCounter);
+        CardView card = view.findViewById(R.id.card);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference viewingprofileDoc = db.collection("posts").document("example post");
-        viewingprofileDoc.get().addOnCompleteListener(getPostTask -> {
+        DocumentReference userId = db.document("accounts/"+
+                Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+
+        DocumentReference postDoc = db.collection("posts").document("example post");
+        postDoc.get().addOnCompleteListener(getPostTask -> executor.execute(()-> {
             if (getPostTask.isSuccessful()) {
                 DocumentSnapshot postDocument = getPostTask.getResult();
                 //if yes populate local object
                 if (postDocument.exists()) {
                     Post p = new Post(postDocument);
-                    postTitle.setText(p.getTitle());
-                    //
-                    String s =p.getBody();
-                    postBody.setText(s);
-                    int upvotesInt = p.retriveUpvotesSet().size();
-                    upvote.append(" "+upvotesInt);
-                    int downvotesInt = p.retriveDownvotesSet().size();
-                    upvote.append(" "+downvotesInt);
+                    String title = p.getTitle();
+                    String body = p.getBody();
+                    String upStr = Integer.toString(p.retriveUpvotesSet().size());
+                    String downStr = Integer.toString(p.retriveDownvotesSet().size());
+
+                    mainHandler.post(()->{
+                        postTitle.setText(title);
+                        postBody.setText(body);
+                        upCount.setText(upStr);
+                        downCount.setText(downStr);
+                        card.setVisibility(View.VISIBLE);
+                        upvote.setOnClickListener(upvote -> {
+                            p.addUpvote(userId,db);
+                            upCount.setText(Integer.toString(p.retriveUpvotesSet().size()));
+                            downCount.setText(Integer.toString(p.retriveDownvotesSet().size()));
+                        });
+                        downvote.setOnClickListener(upvote -> {
+                            p.addDownvote(userId,db);
+                            upCount.setText(Integer.toString(p.retriveUpvotesSet().size()));
+                            downCount.setText(Integer.toString(p.retriveDownvotesSet().size()));
+                        });
+                    });
                 }
             }
+        }));
         });
         return view;
     }
