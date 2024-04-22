4a0e6888e6d72c7091f73aea7ac6c613b498b161
BrownBadger221 <rahulalam72@gmail.com>
2024-04-22 15:41:45 +0100

    Allow user to comment on boards

--- app/app/src/main/java/ul/ie/cs4084/app/CommentFragment.java
+++ app/app/src/main/java/ul/ie/cs4084/app/CommentFragment.java
@@ -1,9 +1,9 @@
-// CommentFragment.java
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,6 +13,19 @@ public class CommentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.comment_fragment, container, false);
+
+        // Retrieve the comment text view
+        TextView commentTextView = view.findViewById(R.id.commentTextView);
+
+        // Get the comment content and poster information from arguments
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            String content = arguments.getString("content");
+            String poster = arguments.getString("poster");
+            // Display the comment content and poster information
+            String commentText = "Poster: " + poster + "\nContent: " + content;
+            commentTextView.setText(commentText);
+        }
 
         // Retrieve the upvote and downvote buttons
         Button upvoteButton = view.findViewById(R.id.upvoteButton);
